f82d77975d47e0540d117bd70b0d2d1e4fc7c4bf
Shishmakova Daria <dary_shishka@mail.ru>
2024-02-29 21:50:34 +0300

    Initial Commit

---
+++ Timetable/TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableApp.swift
+//  Timetable
+//
+//  Created by Дарья Шишмакова on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
