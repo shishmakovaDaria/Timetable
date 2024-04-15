857a8c28aa6c5a9a5815b78e27deca567472086d
Shishmakova Daria <dary_shishka@mail.ru>
2024-04-15 23:52:32 +0300

    added settings screen

--- Timetable/SettingsScreen/SettingsView.swift
+++ Timetable/SettingsScreen/SettingsView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @AppStorage("isDarkMode") private var isDarkMode = false
+    @State var showWebView = false
+    
     var body: some View {
-        Color.white.edgesIgnoringSafeArea(.top)
+        VStack {
+            List {
+                Toggle("Темная тема", isOn: $isDarkMode)
+                    .tint(.ttBlue)
+                    .listRowSeparator(.hidden)
+                HStack {
+                    Button("Пользовательское соглашение") {
+                        showWebView.toggle()
+                    }
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            Spacer()
+            Text("Приложение использует API «Яндекс.Расписания»")
+                .font(.system(size: 12, weight: .regular))
+                .foregroundStyle(.ttBlack)
+            Text("Версия 1.0 (beta)")
+                .font(.system(size: 12, weight: .regular))
+                .foregroundStyle(.ttBlack)
+                .padding([.leading, .top, .trailing], 16)
+                .padding(.bottom, 24)
+        }
+        .fullScreenCover(isPresented: $showWebView) {
+            TermsOfUse(showWebView: $showWebView, url: URL(string: "https://yandex.ru/legal/practicum_offer/")!)
+        }
     }
 }
 
---
+++ Timetable/SettingsScreen/TermsOfUse.swift
@@ -0,0 +1,33 @@
+//
+//  TermsOfUse.swift
+//  Timetable
+//
+//  Created by Дарья Шишмакова on 15.04.2024.
+//
+
+import SwiftUI
+
+struct TermsOfUse: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var showWebView: Bool
+    var url: URL
+    
+    var body: some View {
+        NavigationStack {
+            WebView(url: url)
+                .navigationBarItems(
+                    leading:
+                        ZStack {
+                            Image(systemName: "chevron.left")
+                            Button("") {
+                                dismiss()
+                            }
+                        }
+                )
+                .navigationTitle("Пользовательское соглашение")
+                .navigationBarTitleDisplayMode(.inline)
+                .tint(.ttBlack)
+        }
+       
+    }
+}
